8a3f232be2f71926b9e0f4901e121ab2aefffff4
ashwin vivegan <ashwinjegan6@gmail.com>
2025-03-10 11:52:55 +0530

    :)

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return (
+      <div className="landing-container">
+        <h1>Welcome to ASAP</h1>
+        <p>Father Joke Generator</p>
+      </div>
+    );
+  };
+  
+  export default LandingPage;
+  
