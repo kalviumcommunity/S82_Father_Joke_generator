8262922d42496f1643a1011a0c2bfb75b925408c
ashwin vivegan <ashwinjegan6@gmail.com>
2025-03-19 13:44:06 +0530

    done

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LandingPage from './Landingpage'
+
+function Home() {
+  return (
+    <div>
+
+      
+      
+      <LandingPage></LandingPage>
+    </div>
+  )
+}
+
+export default Home
